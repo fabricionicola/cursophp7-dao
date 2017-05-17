c489a37e94f00f817b7dbd827f114a4ad8efa5c4
Fabricio <fabricionicola@hotmail.com>
2017-05-16 21:22:30 -0300

    métodos de lista

--- 03_index.php
+++ 03_index.php
@@ -17,6 +17,8 @@ require_once("02_config.php");
 //echo json_encode($usuarios);
 
 //AGORA EU SÓ VOU USAR A CLASSE USUÁRIO
+
+/* O LOADBYID TRAZ UM USUÁRIO, COM REFERÊNCAI O ID
 //Estou usando uma referência que já existe no banco de dados. Um usuário que já existe.
 $user2 = new Usuario();
 
@@ -26,4 +28,27 @@ $user2->loadById(6);
 //Agora eu vou exibir o usuário na tela. 
 //Como é um objeto, vai gerar um __toString e vai gerar um json, que foi o estabelecido no arquivo da classe Usuario
 echo $user2;
+*/
+
+//AQUI EU VOU USAR O GETLIST DA CLASSE USUARIO
+//O getList vai trazer uma lista de usuários
+//Como o método está como static, eu não preciso instanciar.
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//NESSE PRÓXIO, ELE CARREGA UMA LISTA DE USUÁRIOS, BUSCANDO PELO LOGIN
+//Esse método é o search, que está na classe Usuario
+//Dentro dos parênteses vai o que queremos buscar, que é pelo login
+//Vou dizer para ele me retornar todos os usuários que comecem com "us"
+//$search = Usuario::search("us");
+//echo json_encode($search);
+
+//NESSE, CARREGA O USUÁRIO USANDO O LOGIN E A SENHA
+//Estamos chamando o método login que está na classe Usuario
+$usuario = new Usuario();
+//Ele espera o login e a senha. É isso que vai dentro do parênteses
+$usuario->login("user2", "&%!@123");
+//Fazendo um echo do $usuário, ele executa um __toString e retorna um json na tela
+echo $usuario;
+
 ?>
--- class/Usuario.php
+++ class/Usuario.php
@@ -67,6 +67,65 @@ class Usuario {
 		}
 		
 	}
+
+//AQUI VAMOS APRENDER A LISTAR DADOS. Eu posso trazer todos os dados de uma tabela ou um usuário por uma determinada condição.
+//Esse é um EXEMPLO de como listar dados. Mais um método.
+	//Vou colocar ele como estático para que eu não precise instanciar esse método no index. É só mais um exercício.
+	public static function getList(){
+		//Precisamos instanciar a classe Sql
+		$sql = new Sql();
+
+		//Agora vamos fazer um return do select
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+//Então, o loadById vai trazer um usuário (o usuário daquele ID), o getList vai trazer uma lista de usuários. 
+
+	}
+
+//AQUI VAMOS FAZER UM SEARCH, DE FORMA ESTÁTICA, vamos buscar um usuário
+//Como parâmetro que será 
+	public static function search($login){
+		$sql = new Sql();
+
+//O %% aqui quer dizer que não importa se o login começa, termina ou contenha letras.
+//Como é um bindParam, precisamos colocar um Id para ele, que no caso aqui é ":SEARCH"
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+//Então, ele vai pegar o parâmetro login e colocar os % e enviar para o parametro que foi preparado, para evitar o sql injection, vai ser a nossa classe que irá inserir as aspas simples.
+			':SEARCH'=>"%".$login."%"
+
+			));
+	}
+
+//AQUI VAMOS FAZER UM MÉTODO PARA OBTER OS DADOS DO USUÁRIO AUTENTICADO.
+//Nós vamos ter que passar o login e a senha para o sistema autenticar, e ai ele vai carregar os dados do usuário
+	//Aqui não pode ser estático.
+	//Esse método irá receber o login e a senha
+	public function login($login, $password){
+		
+		$sql = new Sql();
+
+//Dentro do array nós vamos ter os parâmetros ou os PARAMETERS que a gente colocou dentro da classe Sql
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(":LOGIN"=>$login, ":PASSWORD"=>$password));
+
+//Se trouxer um usuário de fato, ele entra no IF e executa os setters
+//Se não trouxer um usuário, ele cai no else
+		if (count($results)>0){
+			//Crio uma variável para pegar os resultados da linha 0
+			$row = $results[0];
+
+			//Vou pegar os dados que me voltou via associativo e mandar para os setters. 
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		
+		} else {
+			throw new Exception("Login e/ou Senha Inválido(s).");
+		}
+
+	}
+
+
+
 //ATÉ AGORA NÓS SÓ CARREGAMOS OS DADOS DO BANCO PARA O OBJETO.
 //Agora nós queremos vizualizar. Para isso, nós vamos usar um método mágico chamado tostring, que ao inves de mostrar a estrutura do objeto, ele executa o que tem dentro do método chamado __tostring.
 //Então, vamos escrever um método __tostring e vamos dizer para ele executar um json por exemplo
